0217fef68f94ec0af4289bfcce726a81fa13e730
eid iepoh <tornike.dzmanashvili@gmail.com>
2023-06-30 00:54:38 +0400

    removed

--- app.js
+++ app.js
@@ -10,14 +10,13 @@ async function fetcher(url) {
     return data;
     } catch (error) {
         console.log('Error:', error);
-        throw error; // Optionally throw the error
+        throw error; 
     }
 }
 
 async function main() {
     try {
     const result = await fetcher("https://api.ipify.org?format=json");
-    // console.log(result);
     return result;
     } catch (error) {
         console.log('Error:', error);
@@ -28,7 +27,6 @@ const setHtml = (IP) => {
     const elem = document.createElement('div');
     elem.style.fontSize = '25px';
     elem.style.fontWeight = 'bold';
-    elem.style.textAlign = 'center';
     elem.innerText = IP;
     console.log(elem);
     document.body.append(elem);
